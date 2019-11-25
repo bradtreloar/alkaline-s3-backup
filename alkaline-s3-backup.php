be197010a8e9f602566ff667c7c0279fda296717
Brad Treloar <bradley.treloar@gmail.com>
2019-11-25 22:11:38 +1030

    Get region from aws config

--- alkaline-s3-backup.php
+++ alkaline-s3-backup.php
@@ -32,12 +32,24 @@ else {
   throw new \Exception("Unable to locate Drupal root.");
 }
 
-$aws_config = "$home/.aws/alkaline-s3-backup";
-if (file_exists($aws_config)) {
-  $bucket = parse_ini_file($aws_config)['bucket'];
+// Get bucket name from config.
+$config_file = "$home/.aws/alkaline-s3-backup";
+if (file_exists($config_file)) {
+  $config = parse_ini_file($config_file);
+  $bucket = $config['bucket'];
 }
 else {
   throw new \Exception("Unable to locate ~/.aws/alkaline-s3-backup.");
+}
+
+// Get region from AWS config.
+$aws_config_file = "$home/.aws/config";
+if (file_exists($aws_config_file)) {
+  $aws_config = parse_ini_file($aws_config_file);
+  $region = $aws_config['default']['region'];
+}
+else {
+  $region = 'us-east-1';
 }
 
 $archiver = new ArchiveBuilder($drupalRoot, $tmp);
@@ -46,6 +58,7 @@ $archive_filepath = $archiver->buildArchive();
 $s3Client = new S3Client([
   'profile' => 'default',
   'version' => 'latest',
+  'region' => $region,
 ]);
 
 $key = basename($archive_filepath);
