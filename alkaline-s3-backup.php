82206df133f40e38cb2596705cd8496a3e13660c
Brad Treloar <bradley.treloar@gmail.com>
2019-11-25 21:36:47 +1030

    Cleanup and refactor.

--- alkaline-s3-backup.php
+++ alkaline-s3-backup.php
@@ -14,12 +14,12 @@ $cwd = isset($_SERVER['PWD']) && is_dir($_SERVER['PWD'])
 // Set up autoloader.
 $loader = false;
 if (file_exists($autoloadFile = __DIR__ . '/vendor/autoload.php')
-    || file_exists($autoloadFile = __DIR__ . '/../autoload.php')
-    || file_exists($autoloadFile = __DIR__ . '/../../autoload.php')
+  || file_exists($autoloadFile = __DIR__ . '/../autoload.php')
+  || file_exists($autoloadFile = __DIR__ . '/../../autoload.php')
 ) {
-    $loader = include_once($autoloadFile);
+  $loader = include_once($autoloadFile);
 } else {
-    throw new \Exception("Could not locate autoload.php. cwd is $cwd; __DIR__ is " . __DIR__);
+  throw new \Exception("Could not locate autoload.php. cwd is $cwd; __DIR__ is " . __DIR__);
 }
 
 $home = Path::getHomeDirectory();
@@ -30,25 +30,22 @@ if ($drupalFinder->locateRoot($cwd)) {
   $drupalRoot = $drupalFinder->getDrupalRoot();
 }
 else {
-  echo "Unable to locate Drupal root.";
-  exit(1);
+  throw new \Exception("Unable to locate Drupal root.");
 }
 
 $dotenv = new Dotenv("$drupalRoot/..");
 $dotenv->load();
+$bucket = getenv("ALKALINE_S3_BACKUP_BUCKET");
 
 $archiver = new ArchiveBuilder($drupalRoot, $tmp);
 $archive_filepath = $archiver->buildArchive();
 
-$bucket = getenv("ALKALINE_S3_BACKUP_BUCKET");
-$key = basename($archive_filepath);
-
 $s3Client = new S3Client([
   'profile' => 'default',
   'version' => 'latest',
-  'region' => 'ap-southeast-2',
 ]);
 
+$key = basename($archive_filepath);
 $result = $s3Client->putObject([
   'Bucket' => $bucket,
   'Key' => $key,
@@ -56,4 +53,3 @@ $result = $s3Client->putObject([
 ]);
 
 exec("rm $archive_filepath");
-
